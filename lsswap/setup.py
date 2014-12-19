e4a4b4303c2adb214cfbe762294d7a99d1969ba9
Jonathan Gonzalez V <jonathan.abdiel@gmail.com>
2014-12-19 19:26:49 +0000

    Added setup.py file to publish on pypi.python.org

---
+++ lsswap/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='lsswap',
+      version='0.1',
+      py_modules=['lsswap'],
+      url='https://github.com/sxd/sysadmin-tools',
+      author='Jonathan Gonzalez V',
+      author_email='jgonzalez@linets.cl',
+      )
