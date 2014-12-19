115d473ca9febe6915b1d31951d8f10d24116c67
Jonathan Gonzalez V <jonathan.abdiel@gmail.com>
2014-12-19 20:06:18 +0000

    Switch to setuptools and added install_requires field

--- lsswap/setup.py
+++ lsswap/setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 setup(name='lsswap',
-      version='0.1',
+      version='0.2',
       py_modules=['lsswap'],
       url='https://github.com/sxd/sysadmin-tools',
       author='Jonathan Gonzalez V',
       author_email='jgonzalez@linets.cl',
       description='lsswap aim to show all the swap information on a GNU/Linux machine',
-      scripts=['bin/lsswap']
+      scripts=['bin/lsswap'],
+      install_requires=['PrettyTable'],
   )
