6a29ab317680ca2e334c1b5056f7257ef929a425
Jonathan Gonzalez V <jonathan.abdiel@gmail.com>
2014-12-19 19:46:07 +0000

    Added section description and scripts

--- lsswap/setup.py
+++ lsswap/setup.py
@@ -5,4 +5,6 @@ setup(name='lsswap',
       url='https://github.com/sxd/sysadmin-tools',
       author='Jonathan Gonzalez V',
       author_email='jgonzalez@linets.cl',
-      )
+      description='lsswap aim to show all the swap information on a GNU/Linux machine',
+      scripts=['bin/lsswap']
+  )
